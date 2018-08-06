33c441a788b97f49d946be388fe138b6108fc646
Grapevine <jinsw2001@gmail.com>
2018-08-07 05:52:02 +0900

    유저가 파일 이미지 업로드 후 해당 내용 Cloudant 에 저장

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+
+// 소켓io
 io.on('connection', function (socket) {
 
     var user = {
@@ -67,7 +69,6 @@ io.on('connection', function (socket) {
         });
     });
 
-
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
@@ -90,8 +91,7 @@ app.get('/test', function (req, res) {
     });
 });
 
-// 파일 업로드
-
+// 파일 업로드 후 cloudant 저장
 app.post('/upload', upload.single('file'), function(req, res) {
 
     var d = new Date();
@@ -99,10 +99,21 @@ app.post('/upload', upload.single('file'), function(req, res) {
     var info = {
         name : req.file.originalname,
         path : req.file.path,
-        time : d
+        time : d,
+        locationX : req.body.locationX,
+        locationY : req.body.locationY
     };
 
-    res.send(info);
+    var user = nano.use('userimage');
+    // DB_NAME 같은 경우는 대피소가 저장된 db, 사진 저장용 db 따로 해야해서 굳이 환경변수 필요없을듯?
+
+    user.insert(info, function (err, body) {
+        // value / key 값을 집어넣는다
+        if (err) {
+            console.log('에러');
+        }
+    });
+    res.send('user image insert complete');
 });
 
 
@@ -110,6 +121,10 @@ app.post('/upload', upload.single('file'), function(req, res) {
 app.post('/db/insert/shelter', function (req, res) {
     var shelter = nano.use('shelter');
     // DB_NAME 같은 경우는 대피소가 저장된 db, 사진 저장용 db 따로 해야해서 굳이 환경변수 필요없을듯?
+
+    var info = {
+
+    }
 
     shelter.insert(req.body, function (err, body) {
         // value / key 값을 집어넣는다
@@ -121,19 +136,21 @@ app.post('/db/insert/shelter', function (req, res) {
 });
 
 
-// cloudant 값 넣기 , 사용자가 촬영한 사진?
-app.post('/db/insert/user', function (req, res) {
-    var user = nano.use('userimage');
-    // DB_NAME 같은 경우는 대피소가 저장된 db, 사진 저장용 db 따로 해야해서 굳이 환경변수 필요없을듯?
 
-    user.insert(req.body, function (err, body) {
-        // value / key 값을 집어넣는다
-        if (err) {
-            console.log('에러');
-        }
-    });
-    res.send('user image insert complete');
-});
+//  이미지 디비 넣기 테스트, 이미지는 로컬에 저장
+// // cloudant 값 넣기 , 사용자가 촬영한 사진?
+// app.post('/db/insert/user', function (req, res) {
+//     var user = nano.use('userimage');
+//     // DB_NAME 같은 경우는 대피소가 저장된 db, 사진 저장용 db 따로 해야해서 굳이 환경변수 필요없을듯?
+//
+//     user.insert(req.body, function (err, body) {
+//         // value / key 값을 집어넣는다
+//         if (err) {
+//             console.log('에러');
+//         }
+//     });
+//     res.send('user image insert complete');
+// });
 
 
 
